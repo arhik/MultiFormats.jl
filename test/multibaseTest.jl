471e52b0a187d82c842d77fd6a9096ce43133439
arhik <arhik23@gmail.com>
2022-09-29 11:03:15 +0530

    Tests against julia's base64

--- test/multibaseTest.jl
+++ test/multibaseTest.jl
@@ -9,12 +9,14 @@ write(io, inputString)
 seek(io, 0)
 
 buf = read(io)
+but = rand(32321)
 
 encodedString = MultiFormats.multiEncode(:base64, buf)
 
 @testset "Encode and decode" begin
 	@test base64encode(buf) == multiEncode(:base64, buf)
-	@test base64encode(inputString) == multiEncode(:base64, inputString)
+	@test base64decode(encodedString) == multiDecode(:base64, encodedString |> collect .|> UInt8)
+	@test base64encode(buf) == multiEncode(:base64, buf)
 end
 
 # TODO check for large strings and bytes
