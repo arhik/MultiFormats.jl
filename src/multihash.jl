cd8e3a79b7813a03628266e9389de9ffbb969609
arhik <arhik23@gmail.com>
2022-10-03 15:05:32 +0530

    hash wrap and unwrap

--- src/multihash.jl
+++ src/multihash.jl
@@ -1,20 +1,59 @@
 using SHA
+using MultiFormats
+export multiHash, hashWrap, hashUnwrap
 
-abstract type AbstractMultiHash end
+codecs = MultiFormats.codecSymbolList
 
-struct MultiHash{T} <: AbstractMultiHash where T<:AbstractMultiCodec
+for incodec in codecs
+	if codecTag(incodec) == MultiFormats.MULTIHASH
+		if isdefined(SHA, incodec)
+			if incodec == :identity
+				continue
+				# struct IDENTITY_CTX end
+				# quote
+					# function multiHash(a::IDENTITY_CTX, bytes::Vector{UInt8})
+						# identity(bytes)
+					# end
+				# end
+			else
+				ctx = Symbol(uppercase(incodec |> string)*"_CTX")
+				quote
+					function multiHash(cntxt::$ctx, bytes::Vector{UInt8})
+						update!(cntxt, bytes)
+						digest!(cntxt)
+					end
+					function Base.length(a::$ctx)
+						return SHA.digestlen($ctx)
+					end
+					function Base.length(a::Type{$ctx})
+						return SHA.digestlen($ctx)
+					end
+				end |> eval
+			end
+		end
+	end
 end
 
-function wrap(a::MultiHash{T}, b) where T
-	code = getCodeCode(T)
+function multiHash(a::Symbol, bytes::Vector{UInt8})
+	@assert codecTag(a) == MultiFormats.MULTIHASH
+	cntxt = Base.getproperty(SHA, Symbol(uppercase(a |> string)*"_CTX"))()
+	multiHash(cntxt, bytes)
 end
 
-# for codec in codecSymbolList
-	# sName = "MultiHash_"*replace(string(codec), "-"=>"_") |> Symbol
-# 
-	# function wrap(a::MultiHash{T}, b)
-		# code()
-	# end
-# end
+function hashWrap(a::Symbol, bytes::Vector{UInt8})
+	@assert codecTag(a) == MultiFormats.MULTIHASH
+	cntxt = Base.getproperty(SHA, Symbol(uppercase(a|>string)*"_CTX"))()
+	len = length(cntxt) |> UInt |> UVarInt
+	pushfirst!(copy(bytes),  (codecCode(a) |> UVarInt)..., (len)...)
+end
 
+function hashUnwrap(a::Symbol, bytes::Vector{UInt8})
+	@assert codecTag(a) == MultiFormats.MULTIHASH
+	code = codecCode(a) |> UInt |> UVarInt
+	cntxt = Base.getproperty(SHA, Symbol(uppercase(a |> string)*"_CTX"))()
+	len = length(cntxt) |> UInt |> UVarInt
+	@assert bytes[1:(code |> length)] == code "Codec Code doesn't match"
+	@assert bytes[length(code)+1:length(code) + (len |> length)] == len
+	return bytes[(len|>length)+length(code)+1:end]
+end
 
